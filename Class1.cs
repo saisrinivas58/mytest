e816a4a330864d425bbce63babc493669082e901
saisrinivas <vasa.saisrinivas839@gmail.com>
2020-06-24 19:14:04 +0530

    first commit

---
+++ Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mytest123
+{
+    public class TaxService
+    {
+        static public decimal CalculateTax(decimal taxable, string postalCode)
+        {
+            return taxable * (decimal).1;
+        }
+    }
+}
